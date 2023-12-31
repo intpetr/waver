5a1f247e266d64a8ebbd6a89076cf56bb673df3c
Peter <62703463+intpetr@users.noreply.github.com>
2023-12-31 18:06:12 +0100

    Add files via upload
    Initial

---
+++ waving.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+
+void plot(char * arr[], int stretch, int y)
+{
+    for (int i = 0;i<50;i++)
+    {
+        for (int j = 0;j<50;j++)
+        {
+            if (abs((sin(0.5*i)*stretch+14+y/4)-j)<1.5)
+            {
+                arr[i][j]='X';
+            }
+            else
+            {
+                arr[i][j]=' ';
+            }
+        }
+    }
+}
+
+
+void printvis(char * arr[])
+{
+    for (int i = 0;i<50;i++)
+    {
+        for (int j = 0;j<50;j++)
+        {
+            if (j==49)
+            {
+                printf("%c |\n",arr[i][j]);
+            }
+            else
+            {
+                printf("%c ",arr[i][j]);
+            }   
+        }
+    }
+}
+
+int main()
+{
+
+   int (*arrr)[8] = malloc(sizeof *arrr * 7);
+   
+   int r = 50, c = 50, i, j;
+ 
+    char* arr[r];
+    for (i = 0; i < r; i++)
+        arr[i] = (char*)malloc(c * sizeof(char));
+
+    
+    for (int i = 0;i < 3;i++)
+    {
+        arr[i][0]='m';
+        
+    }
+
+
+    int stretch = 2;
+    bool desc = false;
+    while (1)
+    {
+        
+        plot(arr,stretch,stretch);
+        printvis(arr);
+        usleep(100000);
+
+        if (desc)
+        {
+            stretch = stretch-1;
+        }
+        else if (desc == false)
+        {
+            stretch++;
+        }
+
+        if (abs(stretch) == 8)
+        {
+            desc = !desc;
+        }
+    }
+}
+
+
+
